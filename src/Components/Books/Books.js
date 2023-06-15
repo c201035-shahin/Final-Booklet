e3ad2cb8208435761d17bd7207fd62b4a4434404
Mohammad Shahin <c201035shahin.iiuc@gmail.com>
2023-06-15 22:26:20 +0600

    last commit before exam

--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -6,23 +6,24 @@ import './Books.css'
 const Books = () => {
   const booknameRef = useRef()
   const author_nameRef = useRef()
-  const descriptionRef = useRef()
-  const categoryRef = useRef()
   const imgRef = useRef()
+  const descriptionRef = useRef()
   const urlRef = useRef()
-  const dateRef = useRef()
+  const categoryRef = useRef()
 
   const handleBooks = (e) => {
-    const bookname = booknameRef.current.value
-    const author_name = author_nameRef.current.value
-    const description = descriptionRef.current.value
-    const category = categoryRef.current.value
-    const img = imgRef.current.value
-    const url = urlRef.current.value
-    const date = dateRef.current.value
-
-    const newBook = { bookname, author_name, description, category, img, url, date}
-
+    e.preventDefault();
+  
+    const bookname = booknameRef.current.value;
+    const author_name = author_nameRef.current.value;
+    const img = imgRef.current.value;
+    const description = descriptionRef.current.value;
+    const url = urlRef.current.value;
+    const category = categoryRef.current.value;
+    const date = new Date();
+  
+    const newBook = { bookname, author_name, img, description, url, category, date };
+  
     fetch('http://localhost:5000/allBooks', {
       method: 'POST',
       headers: {
@@ -33,25 +34,26 @@ const Books = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.insertedId) {
-          alert('Data is Successfully added in database')
-        }
-        else {
-          alert(data.status)
+          alert('Data is successfully added to the database.');
+        } else {
+          alert(data.status);
         }
       })
-
-    e.preventDefault()
-  }
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('An error occurred while adding data to the database.');
+      });
+  };
 
   return (
-    <section classbookname='booki'>
-        <div classbookname="gridt">
-          <div classbookname="objct">
+    <section className='booki'>
+        <div className="gridt">
+          <div className="objct">
           <Form style={{marginTop:"8vh", alignItems:'center'}}onSubmit={handleBooks}>
             <h1>Add Book</h1>
-          <Form.Group classbookname="mb-3 mt-5" controlId="formBasicauthor_name">
+          <Form.Group className="mb-3 mt-5" controlId="formBasicEmail">
             <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Enter Bookname</Form.Label>
-            <Form.Control classbookname='boxg'
+            <Form.Control className='boxg'
               ref={booknameRef}
               type="text"
               required
@@ -59,9 +61,9 @@ const Books = () => {
               style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
             />
         </Form.Group>
-        <Form.Group classbookname="mb-3" controlId="formBasicauthor_name">
-          <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Enter  Author Name</Form.Label>
-          <Form.Control classbookname='boxg'
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Enter Author Name</Form.Label>
+          <Form.Control className='boxg'
             ref={author_nameRef}
             type="text"
             required
@@ -69,20 +71,54 @@ const Books = () => {
             style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
           />
         </Form.Group>
-        <Form.Group classbookname="mb-3" controlId="formBasicauthor_name">
-          <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Book description</Form.Label>
-          <Form.Control classbookname='boxg'
-            ref={descriptionRef}
-            type="text"
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Enter Image Link</Form.Label>
+          <Form.Control className='boxg'
+            ref={imgRef}
+            type="url"
             required
-            placeholder="Book description"
+            placeholder="Image Link"
             style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
           />
         </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Book Description</Form.Label>
+          <Form.Control className='boxg'
+            ref={descriptionRef}
+            type="text"
+            required
+            placeholder="Book Description"
+            style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label style={{ color:'bisque', fontSize:'20px', fontFamily:'TimesNewRoman' }}>Enter Book Link</Form.Label>
+          <Form.Control className='boxg'
+            ref={urlRef}
+            type="url"
+            required
+            placeholder="Book Link"
+            style={{ fontSize:'20px', fontFamily:'TimesNewRoman' }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label style={{ color: 'bisque', fontSize: '20px', fontFamily: 'TimesNewRoman' }}>Select Book Category</Form.Label>
+        <Form.Control className='boxg'
+          ref={categoryRef}
+          as="select"
+          required
+          style={{ fontSize: '20px', fontFamily: 'TimesNewRoman' }}
+          >
+          <option value="Childrens">Childrens</option>
+          <option value="Literary Fiction">Literary Fiction</option>
+          <option value="Science And Technology">Science And Technology</option>
+          </Form.Control>
+          </Form.Group>
+
         <Button variant="primary" type="submit">Enter Book</Button>
           </Form>
           </div>
-          <div classbookname="objct"><img classbookname="img-fluid" src={gif} alt="about"/></div>
+          <div className="objct"><img className="img-fluid" src={gif} alt="about"/></div>
         </div>
     </section>
   )
